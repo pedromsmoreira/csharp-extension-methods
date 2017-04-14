079fb68b87ae1a9476c12b53575d9c93f167ef3f
Pedro Moreira <pedro.ms.moreira51@gmail.com>
2017-04-14 15:55:41 +0100

    add new folder structure for tests

--- src/Common.Extensions/StringExtensions.cs
+++ src/Common.Extensions/StringExtensions.cs
@@ -16,7 +16,7 @@
 
         public static int ToInt(this string str) => str.IsWhitespace() ? 0 : Convert.ToInt32(str);
 
-        public static short ToShort(this string str) => (short)(str.IsWhitespace() ? 0 : Convert.ToInt16(str));
+        public static short ToShort(this string str) => str.IsWhitespace() ? (short)0 : Convert.ToInt16(str);
 
         public static long ToLong(this string str) => str.IsWhitespace() ? 0 : Convert.ToInt64(str);
 
